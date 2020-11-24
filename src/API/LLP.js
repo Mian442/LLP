2a8465e4f5f5b0501352e759f07230d69e61a861
Mian442 <62568869+Mian442@users.noreply.github.com>
2020-11-24 19:20:12 +0500

    Update LLP.js

--- src/API/LLP.js
+++ src/API/LLP.js
@@ -1,12 +1,12 @@
 const LLP = {
-  apiKey: "AIzaSyB2QH58W38LLEwW7MJSBtPlzsMmbJFGwmY",
-  authDomain: "llpa-78ef8.firebaseapp.com",
-  databaseURL: "https://llpa-78ef8.firebaseio.com",
-  projectId: "llpa-78ef8",
-  storageBucket: "llpa-78ef8.appspot.com",
-  messagingSenderId: "7696719667",
-  appId: "1:7696719667:web:24284bb91d411be3d86846",
-  measurementId: "G-HRJCCHB6KL",
+  apiKey: "AIzaSyCNuJqdw5dCiqSQ5ElRvKlvXwpwHCeM9Uc",
+  authDomain: "goldteams-c0951.firebaseapp.com",
+  databaseURL: "https://goldteams-c0951.firebaseio.com",
+  projectId: "goldteams-c0951",
+  storageBucket: "goldteams-c0951.appspot.com",
+  messagingSenderId: "1078568488382",
+  appId: "1:1078568488382:web:5821fbb85dae38d00ebdef",
+  measurementId: "G-LVFQXC3EZJ",
 };
 
 export default LLP;
